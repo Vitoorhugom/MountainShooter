409d6b841b938ec6ca8129198ddebef60e57bda4
Vitoorhugom <dev.vitorhugo@gmail.com>
2025-06-24 17:03:21 -0300

    Creating player shot Creating enemy shot

--- code/EntityMediator.py
+++ code/EntityMediator.py
@@ -1,5 +1,11 @@
+from code.Const import WIN_WIDTH
 from code.Enemy import Enemy
 from code.Entity import Entity
+from code.PlayerShot import PlayerShot
+
+
+class EnemyShoy:
+    pass
 
 
 class EntityMediator:
@@ -8,6 +14,12 @@ class EntityMediator:
     def __verify_collision_window(ent: Entity):
         if isinstance(ent, Enemy):
             if ent.rect.right < 0:
+                ent.health = 0
+        if isinstance(ent, PlayerShot):
+            if ent.rect.left >= WIN_WIDTH:
+                ent.health = 0
+        if isinstance(ent, EnemyShoy):
+            if ent.rect.right <= 0:
                 ent.health = 0
 
     @staticmethod
